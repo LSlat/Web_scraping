bc34120940cd9c04fefec4bdd7ee10690352d153
LSlat <49042469+LSlat@users.noreply.github.com>
2019-07-13 21:35:36 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 #Dependencies
-#import pymongo
-#conn = 'mongodb://localhost:27017'
-#client = pymongo.MongoClient(conn)
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
@@ -9,14 +6,26 @@ import scrape_mars
 # Create an instance of Flask
 app = Flask(__name__)
 
-# Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# Create connection variable 
+# conn = 'mongodb://localhost:27017'
+
+# Pass connection to the pymongo instance.
+# client = pymongo.MongoClient(conn)
+    #The above line gives an error, 'pymongo is not defined'
+
+# Connect to a database. Will create one if not already available.
+db = client.mars_db
+    #error message: name 'client' is not defined
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
 
-    mars_data = mongo.db.collection.find_one()
+    mars_data = mongo.db.collection.find()
 
     #Return template and data
     return render_template("index.html", mars_data=mars_data)
