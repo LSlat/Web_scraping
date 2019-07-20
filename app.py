255a45130e9f19b48892ef425b81f62eeecb3d99
LSlat <49042469+LSlat@users.noreply.github.com>
2019-07-20 18:13:19 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -9,23 +9,23 @@ app = Flask(__name__)
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
-
+ 
 # Create connection variable 
 # conn = 'mongodb://localhost:27017'
 
 # Pass connection to the pymongo instance.
-# client = pymongo.MongoClient(conn)
-    #The above line gives an error, 'pymongo is not defined'
+# client = PyMongo.MongoClient(conn)
+    #The above line gives an error, 'pymongo is not defined' 
 
 # Connect to a database. Will create one if not already available.
-db = client.mars_db
-    #error message: name 'client' is not defined
+db = mongo.db.mars_db
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
-
-    mars_data = mongo.db.collection.find()
+ 
+    mars_data = mongo.db.collection.find_one()
+    print(mars_data)
 
     #Return template and data
     return render_template("index.html", mars_data=mars_data)
@@ -45,5 +45,3 @@ def scrape():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,11 +1,13 @@
-def scrape():
-    browser = init_browser()
+#Dependencies 
+from splinter import Browser
+from bs4 import BeautifulSoup
 
-    #Dependencies
-    from splinter import Browser
-    from bs4 import BeautifulSoup
+def init_browser():
     executable_path = {'executable_path': 'chromedriver.exe'} 
-    browser = Browser('chrome', **executable_path, headless=True)
+    return Browser('chrome', **executable_path, headless=True)
+ 
+def scrape_info():
+    browser = init_browser()
     
     # URL of NASA page to be scraped
     url_nasa = 'https://mars.nasa.gov/news/'
@@ -20,16 +22,16 @@ def scrape():
 
     #Print the NASA latest news title
     news_title = titles.find('a').text
-    print(news_title)
+    # print(news_title)
 
     #Print the NASA latest news description
     news_p = title_list.find(class_='article_teaser_body').text
-    print(news_p)
+    # print(news_p)
 
     # URL of JPL page to be scraped
     url_jpl = 'https://www.jpl.nasa.gov'
     browser.visit(url_jpl)
-
+   
     html_jpl = browser.html
     soup_jpl = BeautifulSoup(html_jpl, 'html.parser')
 
@@ -39,8 +41,8 @@ def scrape():
 
     #Print the full JPL url to the image page
     featured_image_url_page = url_jpl + (picture['href'])
-    print(featured_image_url_page)
-
+    # print(featured_image_url_page)
+ 
     browser.visit(featured_image_url_page)
 
     html_jpl2 = browser.html
@@ -52,8 +54,8 @@ def scrape():
     pic3 = pic2.find('a')
     # print(pic3['href'])
 
-    #Print the JPL full size image download url
-    featured_image_url = 'https:' + pic3['href']
+    #Print the JPL full size IMAGE DOWNLOAD url
+    featured_image_url = 'http:' + pic3['href']
     print(featured_image_url)
 
     #URL of the Twitter page to be scraped
@@ -65,7 +67,7 @@ def scrape():
 
     #Print the latest Mars weather tweet
     mars_weather = soup_weather.find(class_='js-tweet-text-container').text
-    print(mars_weather)
+    # print(mars_weather)
 
     #Dependency for Pandas
     import pandas as pd
@@ -74,17 +76,17 @@ def scrape():
     url_facts = 'https://space-facts.com/mars/'
 
     #Read the table in the Mars Facts webpage
-    tables = pd.read_html(url_facts)
+    tables = pd.read_html(url_facts)[1]
     # tables
 
-    facts_df = tables[1]
+    facts_df = tables
     facts_df.columns = ['Parameter', 'Mars Fact']
     # facts_df
 
     facts_df.set_index('Parameter', inplace=True)
-    facts_df
+    # facts_df
 
-    facts_df.to_html('mars_facts_table.html')
+    facts_df = facts_df.to_html()
 
     # URL of USGS page to be scraped
     url_usgs1 = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced'
@@ -95,7 +97,7 @@ def scrape():
 
     #Print image title
     cerberus_title = soup_usgs1.find('h2', class_='title').text
-    print(cerberus_title)
+    # print(cerberus_title)
 
     cer_url1 = soup_usgs1.find(class_='downloads')
     # print( cer_url1)
@@ -116,7 +118,7 @@ def scrape():
 
     #Print image title
     schiaparelli_title = soup_usgs2.find('h2', class_='title').text
-    print(schiaparelli_title)
+    # print(schiaparelli_title)
 
     sch_url1 = soup_usgs2.find(class_='downloads')
     # print(sch_url1)
@@ -126,7 +128,7 @@ def scrape():
 
     #Print image url
     schiaparelli_picture = sch_url2.find('a')
-    print(schiaparelli_picture['href'])
+    # print(schiaparelli_picture['href'])
 
     # URL of USGS page to be scraped
     url_usgs3 = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/syrtis_major_enhanced'
@@ -137,7 +139,7 @@ def scrape():
 
     #Print image title
     syrtis_title = soup_usgs3.find('h2', class_='title').text
-    print(syrtis_title)
+    # print(syrtis_title)
 
     syr_url1 = soup_usgs3.find(class_='downloads')
     # print(syr_url1)
@@ -147,7 +149,7 @@ def scrape():
 
     #Print image url
     syrtis_picture = syr_url2.find('a')
-    print(syrtis_picture['href'])
+    # print(syrtis_picture['href'])
 
     # URL of USGS page to be scraped
     url_usgs4 = 'https://astrogeology.usgs.gov/search/map/Mars/Viking/valles_marineris_enhanced'
@@ -158,7 +160,7 @@ def scrape():
 
     #Print image title
     valles_title = soup_usgs4.find('h2', class_='title').text
-    print(valles_title)
+    # print(valles_title)
 
     val_url1 = soup_usgs4.find(class_='downloads')
     # print(val_url1)
@@ -168,7 +170,7 @@ def scrape():
 
     #Print image url
     valles_picture = val_url2.find('a')
-    print(valles_picture['href'])
+    # print(valles_picture['href'])
 
     #Store the data in a dictionary
     mars_data = {
@@ -177,15 +179,15 @@ def scrape():
         'featured_image_url_page':featured_image_url_page,
         'featured_image_url':featured_image_url,
         'mars_weather':mars_weather,
-        'facts_df':facts_df,
+        'facts_df': facts_df,
         'cerberus_title':cerberus_title,
-        'cerberus_picture':cerberus_picture,
+        'cerberus_picture':cerberus_picture['href'],
         'schiaparelli_title':schiaparelli_title,
-        'schiaparelli_picture':schiaparelli_picture,
+        'schiaparelli_picture':schiaparelli_picture['href'],
         'syrtis_title':syrtis_title,
-        'syrtis_picture':syrtis_picture,
+        'syrtis_picture':syrtis_picture['href'],
         'valles_title':valles_title,
-        'valles_picture':valles_picture
+        'valles_picture':valles_picture['href']
     }
 
     #Close the browser after scraping
